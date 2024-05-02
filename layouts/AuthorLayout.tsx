d48ffb2eb66473b8d677feb769a055267ca1a1c2
tanmayshishodia <tanmayshishodia@gmail.com>
2024-05-02 15:34:00 -0400

    initial commit

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
+import Timeline from '@/layouts/Timeline'
 
 interface Props {
   children: ReactNode
@@ -14,11 +15,11 @@ export default function AuthorLayout({ children, content }: Props) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        {/* <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
           </h1>
-        </div>
+        </div> */}
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
             {avatar && (
@@ -44,6 +45,10 @@ export default function AuthorLayout({ children, content }: Props) {
             {children}
           </div>
         </div>
+        {/* <div>
+          <div></div>
+          <Timeline />
+        </div> */}
       </div>
     </>
   )
