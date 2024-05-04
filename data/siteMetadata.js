8d1e109cd87ca2c02d6cf0819d2475720eb2180c
tanmayshishodia <tanmayshishodia@gmail.com>
2024-05-04 00:46:12 -0400

    change to lower case

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'tanmayshishodia',
-  author: 'Tanmay Shishodia',
-  headerTitle: 'Tanmay Shishodia',
+  author: 'tanmay shishodia',
+  headerTitle: 'tanmay shishodia',
   description: 'Personal Website',
   language: 'en-us',
   theme: 'system', // system, dark or light
